6294b78daee63f548ff6fea88131880c84d7a696
patrycjakwiecien <patrycjaapril@gmail.com>
2018-07-03 23:28:25 +0200

    task completed

--- js/main.js
+++ js/main.js
@@ -1,118 +1,29 @@
+//buttons
 
-//// Get the modal
-//var modal = document.getElementById('.playlist_popup');
-//
-//// Get the button that opens the modal
-//var btn = document.getElementById("playlist_button img");
-//
-//// Get the <span> element that closes the modal
-//var span = document.getElementsByClassName("close")[0];
-//
-//// When the user clicks the button, open the modal 
-//btn.onclick = function() {
-//    modal.style.display = "block";
-//}
-//
-//// When the user clicks anywhere outside of the modal, close it
-//window.onclick = function(event) {
-//    if (event.target == modal) {
-//        modal.style.display = "none";
-//    }
-//}
+$(document).ready(function () {
+    $("#back_ico_background-cover-transparent").click(function () {
+        $("#background-covers").hide()
+    });
 
+});
+
+$(document).ready(function () {
+    $("#back_ico_back_playlist").click(function () {
+        $("#playlist").hide()
+    });
+
+});
+
+$(document).ready(function () {
+    $("#back_ico_arrow_playlist").click(function () {
+        $("#playlist").hide()
+    });
+
+});
+
+
+//button play
 
 function myFunction() {
-    var x = document.getElementById("playlist_popup");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+    document.getElementById("active_button").src = "images/Play_active.png";
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//$(function() {
-// $('#playlist_button a').click(function(e) {
-//   e.preventDefault();
-//   animateSlider(this.hash);
-// });
-// function animateSlider(hash) {
-//   if (!$('#playlist div.open').length) {
-//    
-//     if (hash == '#songs') {
-//       openPopup(hash);
-//     }
-//   } else {
-//     
-//     if (hash == '#songs') {
-//       openAndClose(hash)
-//     }
-//   }
-// }
-// function openPopup(hash) {
-//   $(hash + '_popup').slideToggle().addClass('open');
-// }
-// function openAndClose(hash) {
-//   if ($(hash + '_popup').hasClass('open')) {
-//     $($(hash + '_popup')).slideToggle().removeClass();
-//   } else {
-//     $('#playlist div.open').slideToggle().removeClass();
-//     $(hash + '_popup').slideToggle().addClass('open');
-//   }
-// }
-//});
-
-
-
-//$(function() {
-// $('#playlist_button a').click(function(e) {
-//   e.preventDefault();
-//   animateSlider(this.hash);
-// });
-// function animateSlider(hash) {
-//   if (!$('#square div.open').length) {
-//    
-//     if (hash == '#songs') {
-//       openPopup(hash);
-//     }
-//   } else {
-//     
-//     if (hash == '#songs') {
-//       openAndClose(hash)
-//     }
-//   }
-// }
-// function openPopup(hash) {
-//   $(hash + '_popup').slideToggle().addClass('open');
-// }
-// function openAndClose(hash) {
-//   if ($(hash + '_popup').hasClass('open')) {
-//     $($(hash + '_popup')).slideToggle().removeClass();
-//   } else {
-//     $('#square div.open').slideToggle().removeClass();
-//     $(hash + '_popup').slideToggle().addClass('open');
-//   }
-// }
-//});
